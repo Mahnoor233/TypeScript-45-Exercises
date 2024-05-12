a596c8bec9ce33c398655def27ae467712cd2aad
Mahnoor233 <167398746+Mahnoor233@users.noreply.github.com>
2024-05-12 14:35:18 -0700

    Add files via upload

---
+++ 18.five favourite places/main.ts
@@ -0,0 +1,18 @@
+let favouritePlaces : string[]=["saudi-arabia","pakistan","turkey","dubai","azerbhaijan"];
+console.log("Correct sequence:",favouritePlaces);
+// Alphabetical sequence
+console.log("Alphabetical sequence:",[...favouritePlaces].sort());
+//correcet sequence
+console.log("still in correcet sequence:",favouritePlaces);
+//reverse sequence
+console.log("reverse sequence:", [...favouritePlaces].reverse());
+//correcet sequence
+console.log("still in correcet sequence:",favouritePlaces);
+// changing in original array sequence
+console.log("original array reversed:", favouritePlaces.reverse());
+//turn to its oriinal sequnce
+console.log("turn to its oriinal sequnce:",favouritePlaces.reverse());
+// alphabetical sequence
+console.log("sorted in alphabetical sequence:",favouritePlaces.sort());
+// changing in original array sequence to reverse sequence again
+console.log("original array reversed:", favouritePlaces.reverse());
