80600b927d73a154bfe777c49742f5c681b4ac67
Mahnoor233 <167398746+Mahnoor233@users.noreply.github.com>
2024-05-12 23:03:22 -0700

    Add files via upload

---
+++ 24.more contional test/main.ts
@@ -0,0 +1,36 @@
+let car ="car";
+let UppercaseCar ="CAR";
+let five = 5;
+let two = 2;
+//equality
+console.log("is car is equal to car?");
+console.log(car == "car");
+//inequality
+console.log("is car is not equal to car?");
+console.log(car != "car");
+//lower case function
+//true
+console.log("is CAR is equal to car?");
+console.log(UppercaseCar.toLowerCase() =="car");
+//false
+console.log("is CAR is equal to car?");
+console.log(UppercaseCar.toLowerCase() !="car");
+//numerical
+console.log("is five is equal to two?");
+console.log(five == two);
+
+console.log("is five is not equal to two?");
+console.log(five != two);
+
+console.log("is five is greater than two?");
+console.log(five > two);
+
+
+console.log("is five is less than two?");
+console.log(five < two);
+
+console.log("is five is greater than or equal to two?");
+console.log(five >= two);
+
+console.log("is five is less than or equal to two?");
+console.log(five <= two);
